4f15f8999ad3687fd9db829ecb625385dbd56a8b
arzah <69621136+arzah@users.noreply.github.com>
2021-12-29 16:33:21 -0500

    December 29, 2021 4:33 PM

--- process_forms.js
+++ process_forms.js
@@ -406,6 +406,7 @@ function answers() {
     var x = document.createElement("IMG");
 
     var p = document.createElement("FIGCAPTION");
+    var q = document.createTextNode("URL - https://github.com/arzah/imageHosting/blob/master/gryffindor.jpg?raw=true");
     p.appendChild(q);  
     answer.appendChild(p);
 
