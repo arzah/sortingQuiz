02e7fea5d326d61429da1c64ec42838e6d6be8cb
arzah <69621136+arzah@users.noreply.github.com>
2021-12-29 17:49:08 -0500

    December 29, 2021 5:49 PM

--- process_forms.js
+++ process_forms.js
@@ -401,6 +401,30 @@ function answers() {
     y.setAttribute("alt", "Ravenclaw");
     answerbox.appendChild(y);
     }
+    
+    if (Ravenclaw > maxscore && Ravenclaw == Gryffindor || Ravenclaw == Hufflepuf || Ravenclaw == Slytherin) { 
+    var r = document.createTextNode("You have attributes of two houses");
+    answerbox.appendChild(r);
+    var y = document.createElement("IMG");
+    var yTwo = document.createElement("IMG");
+
+    var n = document.createElement("FIGCAPTION");
+    var o = document.createTextNode("Congratulations!");
+    n.appendChild(o);  
+    answer.appendChild(n);
+
+    yTwo.setAttribute("src", "https://github.com/arzah/imageHosting/blob/master/hufflepuff.jpg?raw=true");
+    yTwo.setAttribute("width", "304");
+    yTwo.setAttribute("height", "228");
+    yTwo.setAttribute("alt", "Ravenclaw");
+    answerbox.appendChild(yTwo);
+        
+    y.setAttribute("src", "https://github.com/arzah/imageHosting/blob/master/ravenclaw.png?raw=true");
+    y.setAttribute("width", "304");
+    y.setAttribute("height", "228");
+    y.setAttribute("alt", "Ravenclaw");
+    answerbox.appendChild(y);
+    }
       }else{
         var one = document.getElementById('answer');
         var two = document.createTextNode('You did not fill everything. Refresh and do it again.');
